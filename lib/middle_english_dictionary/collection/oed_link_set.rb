32423a3898991fcc898d85439dd60142e22479ef
Bill Dueber <bill@dueber.com>
2018-03-28 11:40:33 -0400

    Change OED code to pull from single unified file

--- lib/middle_english_dictionary/collection/oed_link_set.rb
+++ lib/middle_english_dictionary/collection/oed_link_set.rb
@@ -7,23 +7,25 @@ module MiddleEnglishDictionary
   module Collection
     class OEDLinkSet < HashArray
 
-      def self.from_directory_of_xml_files(dirstring)
-        err = File.open('errors.txt', 'w:utf-8')
-        oedlist = self.new
-        dir     = Pathname.new(dirstring)
-        dir.children.select{|f| f.to_s =~ /links_.*\.xml/}.each do |f|
-          xml = File.read(f).encode("UTF-8", "Windows-1252")
-          Nokogiri::XML(xml).xpath('/links/link').each do |linknode|
-            oedlink                 = MiddleEnglishDictionary::OEDLink.new_from_nokonode(linknode)
-            if [oedlink.med_id, oedlink.oed_id].all?{|x| x =~ /[A-Za-z0-9]/}
-              oedlist[oedlink.med_id] = oedlink
-            else
-              err.puts linknode.to_xml
-            end
+      def self.from_nokonode(node)
+        err     = File.open('oed_errors.txt', 'w:utf-8')
+        oedlist = node.xpath('/links/link').reduce(self.new) do |acc, linknode|
+          oedlink = MiddleEnglishDictionary::OEDLink.new_from_nokonode(linknode)
+          if [oedlink.med_id, oedlink.oed_id].all? {|x| x =~ /[A-Za-z0-9]/}
+            acc[oedlink.med_id] = oedlink
+          else
+            err.puts linknode.to_xml
           end
+          acc
         end
         oedlist
+      end
+
+      def self.from_xml_file(filename)
+        xml     = File.read(filename).encode("UTF-8", "ISO-8859-1")
+        self.from_nokonode(Nokogiri::XML(xml))
       end
     end
   end
 end
+
